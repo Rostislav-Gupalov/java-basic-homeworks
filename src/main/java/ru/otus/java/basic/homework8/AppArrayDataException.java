147e67c6979f4d180e748594afbe3b769a893105
Ростислав Гупалов <rostislav_gupalov@mail.ru>
2024-09-29 21:18:00 +0300

    Выполнено ДЗ №8.

---
+++ src/main/java/ru/otus/java/basic/homework8/AppArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homework8;
+
+public class AppArrayDataException extends NumberFormatException{
+
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}
