1c3b67eafd5777b02977482d6706143d880016d3
Ростислав Гупалов <rostislav_gupalov@mail.ru>
2024-10-07 16:48:34 +0300

    Ошиюочно замерджил. Файлы очищены

--- src/main/java/ru/otus/java/basic/homework12/MyFileNameFilter.java
+++ src/main/java/ru/otus/java/basic/homework12/MyFileNameFilter.java
@@ -1,18 +1,1 @@
-/*package ru.otus.java.basic.homework12;
-
-import java.io.FilenameFilter;
-import java.io.File;
-
-public class MyFileNameFilter implements FilenameFilter {
-
-    private String ext;
-
-    public MyFileNameFilter(String ext){
-        this.ext = ext.toLowerCase();
-    }
-    @Override
-    public boolean accept(File dir, String name) {
-        return name.toLowerCase().endsWith(ext);
-    }
-}*/
 
