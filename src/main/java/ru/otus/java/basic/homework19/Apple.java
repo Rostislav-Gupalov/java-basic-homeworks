3af53782d1c27304acffa784c3cc45f25f2e15c8
Ростислав Гупалов <rostislav_gupalov@mail.ru>
2024-11-07 23:48:43 +0300

    Выполнено ДЗ № 19

---
+++ src/main/java/ru/otus/java/basic/homework19/Apple.java
@@ -0,0 +1,15 @@
+package ru.otus.java.basic.homework19;
+
+public class Apple extends Fruit{
+
+    //private String name;
+
+    /*public String getName() {
+        return name;
+    }*/
+
+    public Apple (double weight) {
+        super(weight);
+        //this.name = "Apple";
+    }
+}
